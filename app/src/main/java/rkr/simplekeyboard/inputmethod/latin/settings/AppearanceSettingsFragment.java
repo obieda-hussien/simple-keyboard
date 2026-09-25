27b0ff5a0353b2ffd954d309e7d88a7566cbd6c1
Obieda <bis.2410423@eia.edu.eg>
2026-09-25 13:44:25 +0300

    Avoid conflicting legacy color control under Theme Studio

--- app/src/main/java/rkr/simplekeyboard/inputmethod/latin/settings/AppearanceSettingsFragment.java
+++ app/src/main/java/rkr/simplekeyboard/inputmethod/latin/settings/AppearanceSettingsFragment.java
@@ -60,7 +60,8 @@ public final class AppearanceSettingsFragment extends SubScreenFragment {
         final KeyboardTheme theme = KeyboardTheme.getKeyboardTheme(prefs);
         final boolean isSystemTheme = theme.mThemeId != KeyboardTheme.THEME_ID_SYSTEM
                 && theme.mThemeId != KeyboardTheme.THEME_ID_SYSTEM_BORDER;
-        setPreferenceEnabled(Settings.PREF_KEYBOARD_COLOR, isSystemTheme);
+        setPreferenceEnabled(Settings.PREF_KEYBOARD_COLOR,
+                isSystemTheme && !ThemeEngine.isEnabled(getActivity()));
     }
 
     private void setupThemeStudio() {
